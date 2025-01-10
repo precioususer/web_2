2dcce46b95c200c682327f0d35cced11061ee789
Valery Gusev <valerygusev2022@gmail.com>
2025-01-10 16:43:08 +0200

    pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/star-wars/",
     clean: true,
     assetModuleFilename: "assets/image/[name].[ext]",
   },
